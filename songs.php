50fad25fd3bf852d1364f0f7bd99c63720e89392
Darko Kolak <kolakdarko@yahoo.com>
2019-09-18 11:53:32 +0200

    done

--- songs.php
+++ songs.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
 <body>
 <div id="content">
 <h1>Sooooooongs</h1>
 <button type="button" onclick="loadDoc()">Change Content</button>
 </div>
-<div id="songs"></div>
+<div id="songs">
+	<table class='table table-dark' id="tablee"></table>
+</div>
 
 <script>
 function loadDoc() {
@@ -22,12 +27,12 @@ function loadDoc() {
 function myfunction(xml){
 	var i; 
 	var beeke = xml.responseXML;
-	var table = "<tr><th>title</th><th>artist</th><th>country</th><th>genre</th><th>year</th></tr>";
+	var table = "<tr><th><b>TITLE</b></th><th><b>ARTIST</b></th><th><b>COUNTRY</b></th><th><b>GENRE</b></th><th><b>YEAR</b></th></tr>";
 	var darko = beeke.getElementsByTagName("song");
 	for (i=0;i<darko.length;i++){
 		table+="<tr><td>"+darko[i].getElementsByTagName("title")[0].childNodes[0].nodeValue+"</td><td>"+darko[i].getElementsByTagName("artist")[0].childNodes[0].nodeValue+"</td><td>"+darko[i].getElementsByTagName("country")[0].childNodes[0].nodeValue+"</td><td>"+darko[i].getElementsByTagName("genre")[0].childNodes[0].nodeValue+"</td><td>"+darko[i].getElementsByTagName("year")[0].childNodes[0].nodeValue+"</td></tr>";
 	} 
-document.getElementById("songs").innerHTML=table;
+document.getElementById("tablee").innerHTML=table;
 }
 </script>
 </body>
